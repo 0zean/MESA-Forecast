9011eb6e8f3b86abbe21fbbe51325637b002c2b7
Nicholas Picini <npicini@optonline.net>
2023-08-07 21:32:23 -0400

    Updated goertzel.py

--- goertzel.py
+++ goertzel.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.signal import lfilter
 
 
@@ -52,3 +53,31 @@ class Goertzel:
         amp = np.sqrt(ip**2 + qp**2) / (self.N / 2)
         phase = np.arctan2(qp, ip)
         return amp, phase
+
+if __name__=="__main__":
+    # Check to see if Goertzel output = FFT output
+    def wave(amp, freq, phase, x):
+        return amp * np.sin(2*np.pi * freq * x + phase)
+
+    x = np.arange(0, 512)
+    y = wave(1, 1/128, 0, x)
+
+    amp, phase = Goertzel(y, 1/128).goertzel()
+    print(f'Goertzel Amp: {amp:.4f}, phase{phase:.4f}')
+
+    amp, phase = Goertzel(y, 1/128).goertzelFFT()
+    print(f'GoertzelFFT Amp: {amp:.4f}, phase{phase:.4f}')
+
+    amp, phase = Goertzel(y, 1/128).goertzelIIR()
+    print(f'GoertzelIIR Amp: {amp:.4f}, phase{phase:.4f}')
+
+    tt = np.fft.fft(y)
+    test = pd.DataFrame()
+    test['amp'] = np.sqrt(tt.real**2 + tt.imag**2) / (len(y) / 2)
+    test['freq'] = np.fft.fftfreq(tt.size, d=1)
+    test['phase'] = np.arctan2(tt.imag, tt.real)
+
+    max_ = test.iloc[test['amp'].idxmax()]
+    print(f'FFT amp: {max_["amp"]:.4f}, '
+          f'phase: {max_["phase"]:.4f}, '
+          f'freq: {max_["freq"]:.4f}')
