746843cb9a65daa7dc57d255c5e4cfd524e193ac
Nicholas Picini <npicini@optonline.net>
2023-08-08 04:33:23 -0400

    Updated AdaptNCycleGDFT.py

--- src/AdaptNCycleGDFT.py
+++ src/AdaptNCycleGDFT.py
@@ -16,21 +16,21 @@ api = getcwd() + "/api-key.yaml"
 with open(api, "r") as file:
     key = yaml.safe_load(file)
 
+# Retrieve stock data via Alpha Vantage API
+data = av(function='TIME_SERIES_INTRADAY',
+          symbol='SPY',
+          adjusted='true',
+          interval='5min',
+          extended_hours='false',
+          outputsize='full',
+          apikey=key['key'])
 
-time_series = av(function='TIME_SERIES_INTRADAY',
-                 symbol='SPY',
-                 adjusted='true',
-                 interval='5min',
-                 extended_hours='false',
-                 outputsize='full',
-                 apikey=key['key'])
+stock = data.get_extended_data(start_date='2023-05', end_date='2023-08')
 
-data = time_series.get_extended_data(start_date='2023-05', end_date='2023-08')
-
-print(data)
+print(stock)
 
 
-close = data["4. close"]
+close = stock["4. close"]
 
 day = '2023-07-05'
 bar_count = close.resample('D').count().get(day)
