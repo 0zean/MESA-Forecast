c0c3f1b3d3be0b0d23cbf6cc5a68d998dd951153
Nicholas Picini <npicini@optonline.net>
2023-10-26 17:21:10 -0400

    Removing Cython Ext

--- src/ext/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    name='goertzel_cython',
-    ext_modules=cythonize("goertzel_cython.pyx"),
-    include_dirs=[numpy.get_include()]
-)
